0557c92328fd989fdb6e0507c91b26112344780d
cudh <cudh@crmviet.vn>
2023-03-09 21:13:23 +0700

    tao tk

--- src/main/java/TestCode.java
+++ src/main/java/TestCode.java
@@ -1,8 +1,12 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.Scanner;
+
 public class TestCode {
     @Override
     public boolean equals(Object obj) {
+        Scanner sc = new Scanner(System.in);
         return super.equals(obj);
+
     }
 }
